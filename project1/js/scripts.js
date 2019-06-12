91bfcaed9aa28a5e5a40ed1472ae2f67c89e27c4
Trevor Teigen <example@example.com>
2019-06-12 16:08:59 -0700

    modified almost working code

--- project1/js/scripts.js
+++ project1/js/scripts.js
@@ -1,93 +1,30 @@
-// $(function(){
-//   // var leapYear = function(year) {
-//   //   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-//   //     return true;
-//   //   } else {
-//   //     return false;
-//   //   }
-//   // };
-//   // $("form").submit(function(event){
-//   //   event.preventDefault();
-//   //    var userInput = parseInt($("input#year").val());
-//   //    var result = leapYear(userInput);
-//   //
-//   //    $(".year").text(userInput);
-//   //
-//   //    if (!result) {
-//   //      $(".not").text("not");
-//   //    } else {
-//   //      $(".not").text("");
-//   //    }
-//   //    $(".result").show();
-//   // })
-//
-// //business logic
-
-//   $("form").submit(function(event){
-//     event.preventDefault();
-//     var normalText = $("input#normal").val().split('');
-//     var output = translator(normalText);
-//     alert(output);
-//   });
-//
-//
-//
-//
-//   });
-
-
-  // 1st letter vowel -> +=way at the end
-  // 1st letter consonant -> move all consecutive to end +=ay
-  // 1st letter = qu -> move qu to end += ay
-  // y=consonant
-  //
-  //
-  // var j = normal.split(" ");
-  //
-  // normal == This is a sentence
-  // j = ["this","is","a","sentence"]
-
-
-  $(document).ready(function(){
-    $("form").submit(function(event){
-      event.preventDefault();
-      var userInput = $("input#normal").val();
-      var output = isVowel(userInput);
-      var output2 = isConsonants(userInput);
-      $("#result").text(output);
-      $("#result").text(output2);
-
-    });
-  });
-
-
+$(document).ready(function(){
 
     var vowels = ['a','e','i','o','u'];
     var consonants = 'bcdfghjklmnpqrstvwxyz'.split('');
 
-function isVowel(userInput){
+  $("form").submit(function(event){
+    event.preventDefault();
+    $("#result").text(" ");
+    var userInput = $("input#normal").val().split(" ");
+    for (var i = 0; i <= userInput.length; i++){
 
-  for (var index=0;index < vowels.length; index++){
-
-    if (userInput.charAt(0)=== vowels[index]){
-      return userInput+"way";
+      if (vowels.includes(userInput[i].split('')[0])){
+        console.log(userInput[i]);
+      $('#result').append(userInput[i] + 'way'+' ')
       }
-    }
-};
-
-function isConsonants(userInput){
-  for (var index=0; index< consonants.length; index++){
-
-    if(userInput.charAt(0)=== consonants[index]) {
-
-      var userInput = userInput.slice(1)+userInput.charAt(0);
-
-      index = 0;
-      console.log(userInput);
-
-
-    }
-  }
-
-  return userInput+"ay";
-};
+      if (consonants.includes(userInput[i].split('')[0]) && !vowels.includes(userInput[i].split('')[0])) {
+        console.log(userInput[i]);
+      if (userInput[i].charAt(0)=== "q" && userInput[i].charAt(1)=== "u" ) {
+        console.log(userInput[i]);
+      userInput[i] = userInput[i].slice(2)+userInput[i].charAt(0)+userInput[i].charAt(1);
+      }
+      while (consonants.includes(userInput[i].split('')[0])) {
+        console.log(userInput[i]);
+      userInput[i] = userInput[i].slice(1)+userInput[i].charAt(0);
+      }
+      $('#result').append(userInput[i] + 'ay'+ " ");
+      }
+    };
+  });
+});
