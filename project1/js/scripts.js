3f2cd0b8bc154ca46ce9a4fe0a98a83c8a95c6e5
Trevor Teigen <example@example.com>
2019-06-12 16:31:14 -0700

    finally working code

--- project1/js/scripts.js
+++ project1/js/scripts.js
@@ -9,17 +9,16 @@ $(document).ready(function(){
     var userInput = $("input#normal").val().split(" ");
     for (var i = 0; i < userInput.length; i++){
 
-      if (vowels.includes(userInput[i].split('')[0])){
+    if (vowels.includes(userInput[i].split('')[0].toLowerCase())){
         console.log(userInput[i]);
       $('#result').append(userInput[i] + 'way'+' ')
-    } else
-      if (consonants.includes(userInput[i].split('')[0]) && !vowels.includes(userInput[i].split('')[0])) {
+    } else {
         console.log(userInput[i]);
-      if (userInput[i].charAt(0)=== "q" && userInput[i].charAt(1)=== "u" ) {
+      if (userInput[i].charAt(0).toLowerCase()=== "q" && userInput[i].charAt(1).toLowerCase()=== "u" ) {
         console.log(userInput[i]);
       userInput[i] = userInput[i].slice(2)+userInput[i].charAt(0)+userInput[i].charAt(1);
       }
-      while (consonants.includes(userInput[i].split('')[0])) {
+      while (consonants.includes(userInput[i].split('')[0].toLowerCase())) {
         console.log(userInput[i]);
       userInput[i] = userInput[i].slice(1)+userInput[i].charAt(0);
       }
